45e326ae694477c398bd0600a214de2ff672efb4
Filip Procházka <filip@prochazka.su>
2014-09-10 17:04:59 +0200

    init extension

---
+++ src/Kdyby/ElasticSearch/DI/SearchExtension.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Search\DI;
+
+use Kdyby;
+use Nette;
+use Nette\PhpGenerator as Code;
+
+
+
+if (!class_exists('Nette\DI\CompilerExtension')) {
+	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
+	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
+	class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
+}
+
+if (isset(Nette\Loaders\NetteLoader::getInstance()->renamed['Nette\Configurator']) || !class_exists('Nette\Configurator')) {
+	unset(Nette\Loaders\NetteLoader::getInstance()->renamed['Nette\Configurator']); // fuck you
+	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
+}
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class SearchExtension extends Nette\DI\CompilerExtension
+{
+
+	/**
+	 * @var array
+	 */
+	public $defaults = array(
+
+	);
+
+
+
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+		$config = $this->getConfig($this->defaults);
+
+	}
+
+
+	public static function register(Nette\Configurator $configurator)
+	{
+		$configurator->onCompile[] = function ($config, Nette\DI\Compiler $compiler) {
+			$compiler->addExtension('search', new SearchExtension());
+		};
+	}
+
+}
+
